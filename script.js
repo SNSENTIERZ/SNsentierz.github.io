fdd00fffed4865ee3825e8ea13e71364c910ad49
SNSENTIERZ <ethanwatson826@gmail.com>
2026-02-07 07:34:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function showTab(tabId) {
-document.querySelectorAll('.tab').forEach(tab => {
-tab.classList.remove('active');
-});
-document.getElementById(tabId).classList.add('active');
+  document.querySelectorAll('.tab').forEach(tab => {
+    tab.classList.remove('active');
+  });
+
+  document.getElementById(tabId).classList.add('active');
 }
+
